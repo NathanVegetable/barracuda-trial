0b1ca2d6c9af29b657d105d7994f53428ea3f910
NathanVegetable <nathan.dabutt@gmail.com>
2025-11-27 20:35:55 -0800

    Add fallback pathfinding locations

--- src/main/java/com/barracudatrial/game/route/RouteWaypoint.java
+++ src/main/java/com/barracudatrial/game/route/RouteWaypoint.java
@@ -1,5 +1,7 @@
 package com.barracudatrial.game.route;
 
+import java.util.Set;
+
 import lombok.Getter;
 import net.runelite.api.coords.WorldPoint;
 
@@ -10,11 +12,15 @@ public class RouteWaypoint
 	private final int lap;
 	private final WorldPoint location;
 
+	// If the location is not visible, we fall back to these locations (in order of preference)
+	private final Set<WorldPoint> fallbackLocations;
+
 	public RouteWaypoint(WaypointType type, WorldPoint location)
 	{
 		this.lap = 1;
 		this.type = type;
 		this.location = location;
+		this.fallbackLocations = null;
 	}
 
 	public RouteWaypoint(int lap, WaypointType type, WorldPoint location)
@@ -22,6 +28,15 @@ public class RouteWaypoint
 		this.lap = lap;
 		this.type = type;
 		this.location = location;
+		this.fallbackLocations = null;
+	}
+
+	public RouteWaypoint(int lap, WaypointType type, WorldPoint location, Set<WorldPoint> fallbackLocations)
+	{
+		this.lap = lap;
+		this.type = type;
+		this.location = location;
+		this.fallbackLocations = fallbackLocations;
 	}
 
 	@Getter
@@ -32,6 +47,8 @@ public class RouteWaypoint
 		RUM_DROPOFF(7),
 		TOAD_PICKUP(9),
 		TOAD_PILLAR(10),
+		CRYSTAL_MOTE(5),
+		PORTAL(7),
 		PATHFINDING_HINT(0),
 		USE_WIND_CATCHER(0);
 
